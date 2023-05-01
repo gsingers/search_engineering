47215e212ab6eac85e839a1c6af32edbc2a13071
Olzhas Askar <olzhas.askar@gmail.com>
2023-05-01 19:57:42 +0200

    feat: add a refresh interval

--- week1/index.py
+++ week1/index.py
@@ -29,114 +29,110 @@ mappings = {
     "type":"type/text()",
     "shortDescription": "shortDescription/text()",
     "startDate": "startDate/text()",
-"active": "active/text()",
-"regularPrice": "regularPrice/text()",
-"salePrice": "salePrice/text()",
-"shortDescription": "shortDescription/text()",
-"shortDescriptionHtml": "shortDescriptionHtml/text()",
-"longDescription": "longDescription/text()",
-"longDescriptionHtml": "longDescriptionHtml/text()",
-"artistName": "artistName/text()",
-"onSale": "onSale/text()",
-"digital": "digital/text()",
-"frequentlyPurchasedWith": "frequentlyPurchasedWith/*/text()",  # Note the match all here to get the subfields
-"accessories": "accessories/*/text()" ,  # Note the match all here to get the subfields
-"relatedProducts": "relatedProducts/*/text()",  # Note the match all here to get the subfields
-"crossSell": "crossSell/text()",
-"salesRankShortTerm": "salesRankShortTerm/text()",
-"salesRankMediumTerm": "salesRankMediumTerm/text()",
-"salesRankLongTerm": "salesRankLongTerm/text()",
-"bestSellingRank": "bestSellingRank/text()",
-"url": "url/text()",
-"categoryPath": "categoryPath/*/name/text()",  # Note the match all here to get the subfields
-"categoryPathIds": "categoryPath/*/id/text()",  # Note the match all here to get the subfields
-"categoryLeaf": "categoryPath/category[last()]/id/text()",
-"categoryPathCount": "count(categoryPath/*/name)",
-"customerReviewCount": "customerReviewCount/text()",
-"customerReviewAverage": "customerReviewAverage/text()",
-"inStoreAvailability": "inStoreAvailability/text()",
-"onlineAvailability": "onlineAvailability/text()",
-"releaseDate": "releaseDate/text()",
-"shippingCost": "shippingCost/text()",
-"class": "class/text()",
-"classId": "classId/text()",
-"subclass": "subclass/text()",
-"subclassId": "subclassId/text()",
-"department": "department/text()",
-"departmentId": "departmentId/text()",
-"bestBuyItemId": "bestBuyItemId/text()",
-"description": "description/text()",
-"manufacturer": "manufacturer/text()",
-"modelNumber": "modelNumber/text()",
-"image": "image/text()",
-"condition": "condition/text()",
-"inStorePickup": "inStorePickup/text()",
-"homeDelivery": "homeDelivery/text()",
-"quantityLimit": "quantityLimit/text()",
-"color": "color/text()",
-"depth": "depth/text()",
-"height": "height/text()",
-"weight": "weight/text()",
-"shippingWeight": "shippingWeight/text()",
-"width": "width/text()",
-"features": "features/*/text()"  # Note the match all here to get the subfields
-
+    "active": "active/text()",
+    "regularPrice": "regularPrice/text()",
+    "salePrice": "salePrice/text()",
+    "shortDescription": "shortDescription/text()",
+    "shortDescriptionHtml": "shortDescriptionHtml/text()",
+    "longDescription": "longDescription/text()",
+    "longDescriptionHtml": "longDescriptionHtml/text()",
+    "artistName": "artistName/text()",
+    "onSale": "onSale/text()",
+    "digital": "digital/text()",
+    "frequentlyPurchasedWith": "frequentlyPurchasedWith/*/text()",  # Note the match all here to get the subfields
+    "accessories": "accessories/*/text()" ,  # Note the match all here to get the subfields
+    "relatedProducts": "relatedProducts/*/text()",  # Note the match all here to get the subfields
+    "crossSell": "crossSell/text()",
+    "salesRankShortTerm": "salesRankShortTerm/text()",
+    "salesRankMediumTerm": "salesRankMediumTerm/text()",
+    "salesRankLongTerm": "salesRankLongTerm/text()",
+    "bestSellingRank": "bestSellingRank/text()",
+    "url": "url/text()",
+    "categoryPath": "categoryPath/*/name/text()",  # Note the match all here to get the subfields
+    "categoryPathIds": "categoryPath/*/id/text()",  # Note the match all here to get the subfields
+    "categoryLeaf": "categoryPath/category[last()]/id/text()",
+    "categoryPathCount": "count(categoryPath/*/name)",
+    "customerReviewCount": "customerReviewCount/text()",
+    "customerReviewAverage": "customerReviewAverage/text()",
+    "inStoreAvailability": "inStoreAvailability/text()",
+    "onlineAvailability": "onlineAvailability/text()",
+    "releaseDate": "releaseDate/text()",
+    "shippingCost": "shippingCost/text()",
+    "class": "class/text()",
+    "classId": "classId/text()",
+    "subclass": "subclass/text()",
+    "subclassId": "subclassId/text()",
+    "department": "department/text()",
+    "departmentId": "departmentId/text()",
+    "bestBuyItemId": "bestBuyItemId/text()",
+    "description": "description/text()",
+    "manufacturer": "manufacturer/text()",
+    "modelNumber": "modelNumber/text()",
+    "image": "image/text()",
+    "condition": "condition/text()",
+    "inStorePickup": "inStorePickup/text()",
+    "homeDelivery": "homeDelivery/text()",
+    "quantityLimit": "quantityLimit/text()",
+    "color": "color/text()",
+    "depth": "depth/text()",
+    "height": "height/text()",
+    "weight": "weight/text()",
+    "shippingWeight": "shippingWeight/text()",
+    "width": "width/text()",
+    "features": "features/*/text()",  # Note the match all here to get the subfields
+    "startDate": "startDate/text()",
+    "active": "active/text()",
+    "regularPrice": "regularPrice/text()",
+    "salePrice": "salePrice/text()",
+    "shortDescription": "shortDescription/text()",
+    "shortDescriptionHtml": "shortDescriptionHtml/text()",
+    "longDescription": "longDescription/text()",
+    "longDescriptionHtml": "longDescriptionHtml/text()",
+    "artistName": "artistName/text()",
+    "onSale": "onSale/text()",
+    "digital": "digital/text()",
+    "frequentlyPurchasedWith": "frequentlyPurchasedWith/*/text()",  # Note the match all here to get the subfields
+    "accessories": "accessories/*/text()" ,  # Note the match all here to get the subfields
+    "relatedProducts": "relatedProducts/*/text()",  # Note the match all here to get the subfields
+    "crossSell": "crossSell/text()",
+    "salesRankShortTerm": "salesRankShortTerm/text()",
+    "salesRankMediumTerm": "salesRankMediumTerm/text()",
+    "salesRankLongTerm": "salesRankLongTerm/text()",
+    "bestSellingRank": "bestSellingRank/text()",
+    "url": "url/text()",
+    "categoryPath": "categoryPath/*/name/text()",  # Note the match all here to get the subfields
+    "categoryPathIds": "categoryPath/*/id/text()",  # Note the match all here to get the subfields
+    "categoryLeaf": "categoryPath/category[last()]/id/text()",
+    "categoryPathCount": "count(categoryPath/*/name)",
+    "customerReviewCount": "customerReviewCount/text()",
+    "customerReviewAverage": "customerReviewAverage/text()",
+    "inStoreAvailability": "inStoreAvailability/text()",
+    "onlineAvailability": "onlineAvailability/text()",
+    "releaseDate": "releaseDate/text()",
+    "shippingCost": "shippingCost/text()",
+    "class": "class/text()",
+    "classId": "classId/text()",
+    "subclass": "subclass/text()",
+    "subclassId": "subclassId/text()",
+    "department": "department/text()",
+    "departmentId": "departmentId/text()",
+    "bestBuyItemId": "bestBuyItemId/text()",
+    "description": "description/text()",
+    "manufacturer": "manufacturer/text()",
+    "modelNumber": "modelNumber/text()",
+    "image": "image/text()",
+    "condition": "condition/text()",
+    "inStorePickup": "inStorePickup/text()",
+    "homeDelivery": "homeDelivery/text()",
+    "quantityLimit": "quantityLimit/text()",
+    "color": "color/text()",
+    "depth": "depth/text()",
+    "height": "height/text()",
+    "weight": "weight/text()",
+    "shippingWeight": "shippingWeight/text()",
+    "width": "width/text()",
+    "features": "features/*/text()"  # Note the match all here to get the subfields
 }
-'''
-"startDate": "startDate/text()",
-"active": "active/text()",
-"regularPrice": "regularPrice/text()",
-"salePrice": "salePrice/text()",
-"shortDescription": "shortDescription/text()",
-"shortDescriptionHtml": "shortDescriptionHtml/text()",
-"longDescription": "longDescription/text()",
-"longDescriptionHtml": "longDescriptionHtml/text()",
-"artistName": "artistName/text()",
-"onSale": "onSale/text()",
-"digital": "digital/text()",
-"frequentlyPurchasedWith": "frequentlyPurchasedWith/*/text()",  # Note the match all here to get the subfields
-"accessories": "accessories/*/text()" ,  # Note the match all here to get the subfields
-"relatedProducts": "relatedProducts/*/text()",  # Note the match all here to get the subfields
-"crossSell": "crossSell/text()",
-"salesRankShortTerm": "salesRankShortTerm/text()",
-"salesRankMediumTerm": "salesRankMediumTerm/text()",
-"salesRankLongTerm": "salesRankLongTerm/text()",
-"bestSellingRank": "bestSellingRank/text()",
-"url": "url/text()",
-"categoryPath": "categoryPath/*/name/text()",  # Note the match all here to get the subfields
-"categoryPathIds": "categoryPath/*/id/text()",  # Note the match all here to get the subfields
-"categoryLeaf": "categoryPath/category[last()]/id/text()",
-"categoryPathCount": "count(categoryPath/*/name)",
-"customerReviewCount": "customerReviewCount/text()",
-"customerReviewAverage": "customerReviewAverage/text()",
-"inStoreAvailability": "inStoreAvailability/text()",
-"onlineAvailability": "onlineAvailability/text()",
-"releaseDate": "releaseDate/text()",
-"shippingCost": "shippingCost/text()",
-"class": "class/text()",
-"classId": "classId/text()",
-"subclass": "subclass/text()",
-"subclassId": "subclassId/text()",
-"department": "department/text()",
-"departmentId": "departmentId/text()",
-"bestBuyItemId": "bestBuyItemId/text()",
-"description": "description/text()",
-"manufacturer": "manufacturer/text()",
-"modelNumber": "modelNumber/text()",
-"image": "image/text()",
-"condition": "condition/text()",
-"inStorePickup": "inStorePickup/text()",
-"homeDelivery": "homeDelivery/text()",
-"quantityLimit": "quantityLimit/text()",
-"color": "color/text()",
-"depth": "depth/text()",
-"height": "height/text()",
-"weight": "weight/text()",
-"shippingWeight": "shippingWeight/text()",
-"width": "width/text()",
-"features": "features/*/text()"  # Note the match all here to get the subfields
-
-'''
 
 def get_opensearch(the_host="localhost"):
     host = the_host
@@ -214,7 +210,11 @@ def main(source_dir: str, file_glob: str, index_name: str, workers: int, host: s
 
     client = get_opensearch(host)
 
-    #TODO: set the refresh interval
+    client.indices.put_settings(index=index_name, body={
+        "index": {
+            "refresh_interval": refresh_interval
+        }
+    })
     logger.debug(client.indices.get_settings(index=index_name))
     start = perf_counter()
     time_indexing = 0
@@ -226,8 +226,12 @@ def main(source_dir: str, file_glob: str, index_name: str, workers: int, host: s
             time_indexing += the_time
 
     finish = perf_counter()
-    logger.info(f'Done. {docs_indexed} were indexed in {(finish - start)/60} minutes.  Total accumulated time spent in `bulk` indexing: {time_indexing/60} minutes')
-    # TODO set refresh interval back to 5s
+    logger.info(f'Done. {docs_indexed} were indexed in {(finish - start)/60} minutes.  Total accumulated time spent in `bulk` indexing: {time_indexing/60} minutes\n')
+    client.indices.put_settings(index=index_name, body={
+        "index": {
+            "refresh_interval": "5s"
+        }
+    })
     logger.debug(client.indices.get_settings(index=index_name))
 
 if __name__ == "__main__":
