036883307794fd47eebe766f6f581c1dfef8f761
Olzhas Askar <olzhas.askar@gmail.com>
2023-05-01 19:57:52 +0200

    feat: iterate over different setups

--- index-data.sh
+++ index-data.sh
@@ -5,17 +5,20 @@ usage()
   exit 2
 }
 
-DATASETS_DIR="/workspace/datasets"
-PYTHON_LOC="/workspace/search_engineering"
-WEEK="utilities" #Default indexing is in utilities
-PRODUCTS_JSON_FILE="bbuy_products.json"
+CURRENT_DIR=$(cd "$(dirname "$0")/."; pwd)
+
+DATASETS_DIR="$CURRENT_DIR/downloads"
+WEEK="$CURRENT_DIR/week1" #Default indexing is in utilities
+PRODUCTS_JSON_FILE="$WEEK/bbuy_products.json"
 HOST="localhost"
 INDEX_NAME="bbuy_products"
-LOGS_DIR="/workspace/logs"
+LOGS_DIR="$CURRENT_DIR/logs"
 WORKERS=8
 MAX_DOCS=2000000
 BATCH_SIZE=200
-while getopts ':p:b:k:i:o:g:y:d:m:w:h' c
+REFRESH_INTERVAL=-1
+
+while getopts ':p:b:k:i:o:g:d:m:w:h' c
 do
   case $c in
     b) BATCH_SIZE=$OPTARG ;;
@@ -26,42 +29,44 @@ do
     m) MAX_DOCS=$OPTARG ;;
     o) HOST=$OPTARG ;;
     p) PRODUCTS_JSON_FILE=$OPTARG ;;
+    r) REFRESH_INTERVAL=$OPTARG ;;
     w) WORKERS=$OPTARG ;;
-    y) PYTHON_LOC=$OPTARG ;;
     h) usage ;;
     [?])
       echo "Invalid option: -${OPTARG}"
       usage ;;
   esac
 done
+
 shift $((OPTIND -1))
 
-mkdir $LOGS_DIR
+mkdir -p $LOGS_DIR
 
-cd $PYTHON_LOC || exit
-cd $WEEK || exit
-echo "Running python scripts from $PYTHON_LOC/$WEEK"
-
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
 set -x
 
-#pyenv activate search_eng
-echo "Creating index settings and mappings"
-if [ -f $PRODUCTS_JSON_FILE ]; then
-  echo " Product file: $PRODUCTS_JSON_FILE"
-  curl -k -X PUT -u admin:admin  "https://$HOST:9200/$INDEX_NAME" -H 'Content-Type: application/json' -d "@$PRODUCTS_JSON_FILE"
-  if [ $? -ne 0 ] ; then
-    echo "Failed to create index with settings of $PRODUCTS_JSON_FILE"
-    exit 2
-  fi
+cd $WEEK
 
-  if [ -f index.py ]; then
-    echo "Indexing product data in $DATASETS_DIR/product_data/products and writing logs to $LOGS_DIR/index.log"
-    nohup python index.py -w $WORKERS -m $MAX_DOCS -b $BATCH_SIZE -s "$DATASETS_DIR/product_data/products" -o "$HOST" -i "$INDEX_NAME" > "$LOGS_DIR/index.log" &
+for refresh in "-1" "1s" "60s"; do
+  echo "Deleting Products"
+  curl -sk -X DELETE -u admin:admin https://localhost:9200/bbuy_products
+
+  echo "Creating index settings and mappings"
+  if [ -f $PRODUCTS_JSON_FILE ]; then
+    echo " Product file: $PRODUCTS_JSON_FILE"
+    curl -k -X PUT -u admin:admin  "https://$HOST:9200/$INDEX_NAME" -H 'Content-Type: application/json' -d "@$PRODUCTS_JSON_FILE"
     if [ $? -ne 0 ] ; then
-      echo "Failed to index products"
+      echo "Failed to create index with settings of $PRODUCTS_JSON_FILE"
       exit 2
     fi
+
+    if [ -f index.py ]; then
+      echo "Indexing product data in $DATASETS_DIR/product_data/products and writing logs to $LOGS_DIR/index.log"
+      python index.py -w $WORKERS -r "$refresh" -m $MAX_DOCS -b $BATCH_SIZE -s "$DATASETS_DIR/product_data/products" -o "$HOST" -i "$INDEX_NAME" >> "$LOGS_DIR/index.log" &
+      wait
+      if [ $? -ne 0 ] ; then
+        echo "Failed to index products"
+        exit 2
+      fi
+    fi
   fi
-fi
+done
